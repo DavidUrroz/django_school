e217abf74297dc43c7f3c126f486d615415fc334
David Urroz <davidurrozp@gmail.com>
2023-08-09 01:22:21 -0600

    changing school's contact number field (model) from integer to charfield

--- escuela/models.py
+++ escuela/models.py
@@ -6,7 +6,7 @@ class Escuela(models.Model):
     name = models.CharField(max_length=100)
     location = models.CharField(max_length=300)
     email = models.EmailField()
-    contact_number = models.IntegerField()
+    contact_number = models.CharField(max_length=20)
 
     def __str__(self):
         return self.name
