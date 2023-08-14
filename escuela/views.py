bca81bf151b653151c64a0b5adee35153009ca45
David Urroz <davidurrozp@gmail.com>
2023-08-14 02:42:06 -0600

    Tarea 2 permissions viewsets

--- escuela/views.py
+++ escuela/views.py
@@ -1,5 +1,7 @@
 # from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
+from rest_framework import permissions
+# from .permissions import IsOwnerOrReadOnly
 from .models import Escuela, Estudiante, Maestro
 from .serializers import EscuelaSerializer, EstudianteSerializer, MaestroSerializer
 # Create your views here.
@@ -7,11 +9,13 @@ from .serializers import EscuelaSerializer, EstudianteSerializer, MaestroSeriali
 class EscuelaViewset(ModelViewSet):
     queryset = Escuela.objects.all()
     serializer_class = EscuelaSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class EstudianteViewset(ModelViewSet):
     queryset = Estudiante.objects.all()
     serializer_class = EstudianteSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
 class MaestroViewset(ModelViewSet):
